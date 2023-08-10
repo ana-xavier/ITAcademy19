62dd356f01b8beb267f48e38cd3aaaea2ce70dfb
Ana Xavier <anacarolina.oxavier@gmail.com>
2023-08-10 16:47:22 -0300

    Explorando Array

---
+++ Aula05/03_Array/Program.cs
@@ -0,0 +1,18 @@
+﻿// --> Compreendendo Arrays
+
+int? [] vetor = new int?[10];
+Random rd = new Random();
+
+for(int i = 0; i < vetor.Length; i++){
+    vetor[i] = rd.Next(101);
+}
+
+for(int i = 0; i < vetor.Length; i++){
+    Console.WriteLine($"[{i}]: {vetor[i]}");
+}
+
+Console.WriteLine("| ======================= |");
+// --> No ForEach não é possível modificar o valor, apenas consultar
+foreach(int valor in vetor){
+    Console.WriteLine($"fe -> {valor}");
+}
